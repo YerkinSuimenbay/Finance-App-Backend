28c40a304cccaa3fcfc38163291622c78958cb20
YerkinSuimenbay-M <yerkin.suimenbay@gmail.com>
2022-03-22 04:57:36 +0600

    2103

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,6 @@
 const express = require('express')
-const { register, login, deleteAccount } = require('../controllers/auth')
 const { updateUserSettings } = require('../controllers/user')
 const router = express.Router()
-
-const authenticateUser = require('../middleware/authentication')
-
-router.post('/register', register)
-router.post('/login', login)
-router.delete('/', authenticateUser, deleteAccount)
 
 router.patch('/settings', updateUserSettings)
 
